cdbaf049f2f7bfa090b138d579954dfb431d0401
LordPass1 <dieg.godoy@duocuc.cl>
2023-11-19 22:22:03 -0300

    intento de validación
    intento de validar el inicio de sesion

--- uber/src/app/modules/registro/registro.page.ts
+++ uber/src/app/modules/registro/registro.page.ts
@@ -8,6 +8,13 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
  
+  nombre: string = ''
+  apellido: string = ''
+  contrasena: string = ''
+  con_contrasena: string = ''
+  correo: string = '' 
+  respuesta: string = ''
+  
   constructor() {}
 
   ngOnInit() {
