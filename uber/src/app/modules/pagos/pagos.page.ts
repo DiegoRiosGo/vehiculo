2aadc12647e8854f8404cd3e50cb17c2884275e8
DiegoRiosGo <die.rios@duocuc.cl>
2024-05-08 18:29:40 -0400

    actualización consulta

--- uber/src/app/modules/pagos/pagos.page.ts
+++ uber/src/app/modules/pagos/pagos.page.ts
@@ -8,6 +8,8 @@ import { FlowService } from 'src/app/services/flow.service';
 })
 export class PaymentPage {
 
+  pagosRecibidos: any[];
+
   constructor(private flowService: FlowService) {}
 
   enlaceDePago: string = "https://www.flow.cl/btn.php?token=27mhkwc";
@@ -16,6 +18,18 @@ export class PaymentPage {
     window.location.href = this.enlaceDePago;
   }
 
+  ngOnInit() {
+    this.obtenerPagosRecibidos();
+  }
+
+  async obtenerPagosRecibidos() {
+    try {
+      const response = await this.flowService.obtenerPagosRecibidos();
+      this.pagosRecibidos = response.data; // Ajusta esto según la estructura real de la respuesta de la API de Flow
+    } catch (error) {
+      console.error('Error al obtener los pagos recibidos:', error);
+    }
+  }
   
 }
 
